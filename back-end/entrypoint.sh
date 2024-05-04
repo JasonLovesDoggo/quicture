b344ed650f23ee1fa103626cb48857ccba3aee0e
Jason Cameron <git@jasoncameron.dev>
2024-05-04 18:25:45 +0000

    add uvicorn logs
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-fastapi run main.py --proxy-headers --port 8300 
+# fastapi run main.py --proxy-headers --port 8300 
+uvicorn main:app --port 8300 --log-config /log_format.yml  --proxy-headers 
--- back-end/utils/gcp/__init__.py
+++ back-end/utils/gcp/__init__.py
@@ -6,12 +6,13 @@ BUCKET_NAME = 'quickshare-images'
 CREDS_FILE = '/home/json/.secrets/gdsc/creds.json' 
 if os.getenv("ENVIROMENT", "local") == "production":
     CREDS_FILE = '/creds.json'
+
+
 async def upload_to_bucket(room_hash: str, file_name: str, file_obj: bytes):
     """ Upload csv files to bucket. """
     async with aiohttp.ClientSession() as session:
         storage = Storage(service_file=CREDS_FILE, session=session) 
         status = await storage.upload(BUCKET_NAME, f'{room_hash}/{file_name}', file_obj)
-        print(status)
         return status['selfLink']
         
 
