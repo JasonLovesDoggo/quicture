90679875b5db461e6066a9059a3e8f7a522b7b85
Jason Cameron <git@jasoncameron.dev>
2024-05-04 18:27:27 +0000

    fix a typo
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # fastapi run main.py --proxy-headers --port 8300 
-uvicorn main:app --port 8300 --log-config /log_format.yml  --proxy-headers 
+uvicorn main:app --port 8300 --log-config /log-format.yml  --proxy-headers 
