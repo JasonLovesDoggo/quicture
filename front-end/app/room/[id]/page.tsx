a68abc381e027befb00d0d7235e432aa05c9a80d
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-05-04 15:37:37 -0400

    Added a checkbox for

--- front-end/app/room/[id]/page.tsx
+++ front-end/app/room/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function Page({ params }: { params: { slug: string } }) {
       <div className=" relative flex justify-start pl-7">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <p className="text-2xl sm:text-4xl md:text-6xl font-bold z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 fixed ">
-          Quickture
+          Quicture
         </p>
       </div>
       <RoomBody />
