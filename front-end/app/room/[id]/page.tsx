1a11bee900fbf2590ed2aef0cc7d683f347f29e9
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2024-05-05 02:42:21 -0400

    on Captain json's special request <3

--- front-end/app/room/[id]/page.tsx
+++ front-end/app/room/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import RoomBody from "@/components/shared/RoomBody";
 import SlideMotionDiv from "@/components/ui/slide-motion-div";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 import React from "react";
 
@@ -11,11 +12,13 @@ export default function Page({ params }: { params: { slug: string } }) {
     <div className="dark md:h-screen h-screen w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
       <div className=" relative flex justify-start pl-7">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <SlideMotionDiv delay={0.5} fadeDirection="down">
-          <p className="text-2xl sm:text-4xl md:text-6xl font-bold z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 fixed ">
-            Quicture
-          </p>
-        </SlideMotionDiv>
+        <Link href="/">
+          <SlideMotionDiv delay={0.5} fadeDirection="down">
+            <p className="text-2xl sm:text-4xl md:text-6xl font-bold z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 mt-8 fixed ">
+              Quicture
+            </p>
+          </SlideMotionDiv>
+        </Link>
       </div>
       <RoomBody id={id as string} />
     </div>
