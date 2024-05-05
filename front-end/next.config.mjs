3d838d0e65c8fc53a45ae92848b6e9e309b3ef47
Jason Cameron <git@jasoncameron.dev>
2024-05-05 07:54:53 +0000

    updated docker configs
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: 'www.googleapis.com'}]},     reactStrictMode: false};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "www.googleapis.com" }],
+  },
+  reactStrictMode: false,
+  output: "standalone",
+};
 
 export default nextConfig;
